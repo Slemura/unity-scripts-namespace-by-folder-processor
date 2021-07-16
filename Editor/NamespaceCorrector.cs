cf448aab5d14c88cc6c583e5f38045d458b00927
Roman <rpugamedev@gmail.com>
2021-07-16 22:25:17 +0600

    - Remove unused refs

--- Editor/NamespaceCorrector.cs
+++ Editor/NamespaceCorrector.cs
@@ -1,10 +1,6 @@
 using System;
+using UnityEngine;
 using UnityEditor;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using UniTools;
 
 namespace com.rpdev.usnfp.Editor {
     
